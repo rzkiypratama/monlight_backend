7cfd9b9a9842e9e353dd31d73d11dda00f22eb49
RIZKY_PUTRA_PRATAMA <pratamabusiness@gmail.com>
2022-10-25 15:24:58 +0800

    update

--- src/Middleware/upload.js
+++ src/Middleware/upload.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10000000} // validasi "file to large, max 1mb"
+  limits: { fileSize: 1000000} // validasi "file to large, max 1mb"
 });
 
 
